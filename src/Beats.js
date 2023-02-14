784b9fe5884ff36792373139f0e423c724ff3ff5
nicoben1x <88980384+nicoben1x@users.noreply.github.com>
2023-02-14 17:45:55 -0300

    Se implemento funcionalidad en tracks

--- src/Beats.js
+++ src/Beats.js
@@ -3,34 +3,46 @@ import BeatsCard from "./BeatsCard";
 import song1 from "./audio/song1.mp3";
 import song2 from "./audio/song2.mp3";
 import song3 from "./audio/song3.mp3";
+import { useState } from "react";
 
 function Beats() {
+
+
+
+
     return (
         <div className="beats-div">
             <h1 className="beats-h1">Tracks</h1>
+           
             <div className="beats-space">
                 <BeatsCard
+                    id={1}
                     image="https://cdn.epidemicsound.com/player/20230210.3-2721044e7f8924c2232153450461c8e143316057/8e3677e50f7ed74753d367ce5e3bb21d-384.png"
                     title="Oranga War"
                     genre="Trap"
                     bpm={120}
                     song={song1}
+                   
                 />
 
                 <BeatsCard
+                    id={2}
                     image="https://cdn.epidemicsound.com/player/20230210.3-2721044e7f8924c2232153450461c8e143316057/d70f6dc1c97191b06d091d11f2eb7444-384.jpg"
                     title="Peace Machine"
                     genre="Reggeaton"
                     bpm={90}
                     song={song2}
+                 
                 />
 
                 <BeatsCard
+                    id={3}
                     image="https://cdn.epidemicsound.com/player/20230210.3-2721044e7f8924c2232153450461c8e143316057/c4e6162ea5f06778552179b53edb4cca-384.jpg"
                     title="Big Bana"
                     genre="Rap"
                     bpm={85}
                     song={song3}
+                    
                 />
 
             </div>
--- src/BeatsCard.js
+++ src/BeatsCard.js
@@ -4,49 +4,72 @@ import pauselogo from "./assets/pause.png"
 import { useRef } from 'react';
 
 
-function BeatsCard({image,title,genre,bpm,song}){
+function BeatsCard({ id, image, title, genre, bpm, song, onButtonClick }) {
 
-    const audioRef = useRef(null);
     const playRef = useRef(null);
     const pauseRef = useRef(null);
+    const audioRef = useRef(null);
 
-    const handlePlay = () => {
-        if (audioRef.current) {
-          if (audioRef.current.paused) {
-            audioRef.current.play();
-            playRef.current.classList.add("nonedis");
-            pauseRef.current.classList.remove("nonedis");
 
-          } else {
-            audioRef.current.pause();
-            playRef.current.classList.remove("nonedis");
-            pauseRef.current.classList.add("nonedis");
-          }
-        }
-      };
+    function clickPlay() {
+
+
+        let audioElements = document.querySelectorAll('.audioSongg');
+        let imagePlayers = document.querySelectorAll('.img-play');
+        let imagePauses = document.querySelectorAll('.img-pause');
+
+        audioElements.forEach(function (audio) {
+            if (!audio.paused) {
+                audio.pause(); // Detener la reproducción del audio
+            }
+        });
+
+        imagePlayers.forEach(function (image) {
+            image.classList.remove("nonedis")
+        });
+        imagePauses.forEach(function (image) {
+            image.classList.add("nonedis")
+        });
+
+
+
+        audioRef.current.play();
+        playRef.current.classList.add("nonedis");
+        pauseRef.current.classList.remove("nonedis");
+    }
+
+    function clickPause() {
+        audioRef.current.pause();
+
+        playRef.current.classList.remove("nonedis");
+        pauseRef.current.classList.add("nonedis");
+    }
+
+
+
 
 
 
     return (
         <div className="beatscard-div">
-            <img className="beatscard-img" src={image}/>
-            <img ref={playRef} onClick={handlePlay} className="beatscard-playimg img-play" src={playlogo}/>
-            <img ref={pauseRef} onClick={handlePlay} className="beatscard-playimg img-pause nonedis" src={pauselogo}/>
-            <audio ref={audioRef} className="audioSong" controls>
-                <source
-                src={song}
-                type="audio/mpeg"
+            <img className="beatscard-img" src={image} />
+            <img ref={playRef} onClick={() => clickPlay()} className="beatscard-playimg img-play" src={playlogo} />
+            <img ref={pauseRef} onClick={() => clickPause()} className="beatscard-playimg img-pause nonedis" src={pauselogo} />
+            <h1 className="beatscard-title" >{title}</h1>
+            <h2 className="beatscard-genre">{genre} - {bpm} BPM</h2>
+            <audio ref={audioRef} className="audioSongg" controls>
+                <source className="audioSource"
+                    src={song}
+                    type="audio/mpeg"
                 />
                 Tu navegador no soporta este audio
             </audio>
-            <h1 className="beatscard-title" >{title}</h1>
-            <h2 className="beatscard-genre">{genre} - {bpm} BPM</h2>
 
         </div>
 
     );
 
-  
+
 
 
 
