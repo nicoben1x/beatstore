d3d331e228a6a8474250837508ac74ddae5d182c
nicoben1x <88980384+nicoben1x@users.noreply.github.com>
2023-02-14 18:09:55 -0300

    Se trabajo en el footer

--- src/About.js
+++ src/About.js
@@ -5,6 +5,8 @@ function About(){
         <div className="about-div">
             
             <div className="about-space">
+            <p className="about-frase">"Sin música, la vida sería un error". <a className="about-textmark">Friederich Nietzcche.</a></p>
+                <p className="derechos">© 2023 <a className="about-textmark">Aire Beatz</a> Music Producer. Todos los derechos reservados.</p>
 
             </div>
 
